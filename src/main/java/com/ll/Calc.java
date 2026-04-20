65edc4e597a21c77f6afce9cdc1191f1bacf15d4
SHINJAEHEE-DEV <asjh011969@gmail.com>
2026-04-20 16:50:00 +0900

    t5 green

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -2,12 +2,19 @@ package com.ll;
 
 public class Calc {
     public static int run(String expression) {
-        String[] expressionBits = expression.split(" \\+ ");
+        String[] expressionBits = expression.split(" ");
 
         int num1 = Integer.parseInt(expressionBits[0]);
-        int num2 = Integer.parseInt(expressionBits[1]);
-
-        return num1 + num2;
+        int num2 = Integer.parseInt(expressionBits[2]);
+        String operationSymbol = expressionBits[1];
+        int result = 0;
+        switch (operationSymbol) {
+            case "+":
+                result = num1 + num2;
+            case "-":
+                result = num1 - num2;
+        }
+        return result;
     }
 
 }
