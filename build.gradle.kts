6c7566cdbf5a481041dfbb86869404d73bcd5be0
SHINJAEHEE-DEV <asjh011969@gmail.com>
2026-04-20 16:43:08 +0900

    t5 red

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,9 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.assertj:assertj-core:3.27.3")
+    // JUnit 5 테스트 실행을 위한 런처 추가
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
 }
 
 tasks.test {
