550ef16a693f3501cd945bfb77c506b0027b0a62
SHINJAEHEE-DEV <asjh011969@gmail.com>
2026-04-20 17:41:53 +0900

    t8 Green

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -6,11 +6,15 @@ public class Calc {
 
         int num1 = Integer.parseInt(expressionBits[0]);
         int num2 = Integer.parseInt(expressionBits[2]);
+        int num3 = Integer.parseInt(expressionBits[4]);
+
         String operationSymbol = expressionBits[1];
+
         int result = 0;
         switch (operationSymbol) {
             case "+":
-                result = num1 + num2;
+                result = num1 + num2 + num3;
+                break;
             case "-":
                 result = num1 - num2;
         }
