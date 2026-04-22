18d0c8e3f4488a5ffbab6f4ae97b9140ea40aa1b
SHINJAEHEE-DEV <asjh011969@gmail.com>
2026-04-22 17:32:13 +0900

    t13 red

--- src/test/java/com/ll/CalcTest.java
+++ src/test/java/com/ll/CalcTest.java
@@ -72,6 +72,14 @@ public class CalcTest {
     void t12() {
         assertThat(Calc.run("10 * 10")).isEqualTo(100);
     }
+
+    @Test
+    @DisplayName("10 * -10 == -100")
+    void t13() {
+        assertThat(Calc.run("10 * -10")).isEqualTo(-100);
+    }
+
+
 }
 
 
