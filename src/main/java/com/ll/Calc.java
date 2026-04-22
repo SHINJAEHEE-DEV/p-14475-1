347a5f5ac422f8da9de55e4c34264074cc4544d1
SHINJAEHEE-DEV <asjh011969@gmail.com>
2026-04-22 23:55:46 +0900

    t14~17 green // ai힌트를 받아서 재귀적으로 구현

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -1,31 +1,49 @@
 package com.ll;
 
-import java.util.Arrays;
-
 public class Calc {
     public static int run(String expression) {
         int result = 0;
-        String[] expressionArr = expression.split(" ");
-        int[] numbers =  Arrays.stream(expressionArr)
-                .filter(s -> s.matches("-?\\d+"))
-                .mapToInt(Integer::parseInt)
-                .toArray();
-        String[] operationSymbols = Arrays.stream(expressionArr)
-                .filter(s -> s.matches("[+-/*]"))
-                .toArray(String[]::new);
-        result = numbers[0];
-        int count = 1;
-        for(String op : operationSymbols){
-            switch (op){
-                case "+" -> result += numbers[count];
-                case "-" -> result -= numbers[count];
-                case "*" -> result *= numbers[count];
-                case "/" -> result /= numbers[count];
-            }
-            count ++;
+        result = devide(expression);
+
+        return result;
+    }
+    public static int devide(String expression) {
+
+        // 1. 가장 뒤에 있는 + 또는 -를 찾는다 (lastIndexOf)
+        if(expression.lastIndexOf(" + ") != -1) {
+            int index = expression.lastIndexOf("+");
+            String left = expression.substring(0, index);
+            String right = expression.substring(index + 1);
+
+            return devide(left) + devide(right);
         }
 
-       return result;
+        if(expression.lastIndexOf(" - ") != -1) {
+            int index = expression.lastIndexOf("-");
+            String left = expression.substring(0, index);
+            String right = expression.substring(index + 1);
+
+            return devide(left) - devide(right);
+        }
+
+        // 3. 1번이 없다면, 가장 뒤에 있는 * 또는 /를 찾는다.
+        if(expression.lastIndexOf(" * ") != -1) {
+            int index = expression.lastIndexOf("*");
+            String left = expression.substring(0, index);
+            String right = expression.substring(index + 1);
+
+            return devide(left) * devide(right);
+        }
+        if(expression.lastIndexOf(" / ") != -1) {
+            int index = expression.lastIndexOf("/");
+            String left = expression.substring(0, index);
+            String right = expression.substring(index + 1);
+
+            return devide(left) / devide(right);
+        }
+        return Integer.parseInt(expression.trim());
     }
+
+
 
 }
